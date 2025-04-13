d1fd97055f907fdb9c194f0fe0119fcde397f20d
Ethan Jin <ejin05@csu.fullerton.edu>
2025-04-12 19:51:43 -0700

    Updated with getter/setter functions

--- src/sphere.h
+++ src/sphere.h
@@ -4,6 +4,7 @@
 //Gonna see if I can test it here?
 #include <raylib.h>
 #include <raymath.h>
+#include <vector>
 
 //class that creates a general 3d sphere shape for celestial bodies
 //data we are grabbing: position and velocity
@@ -12,21 +13,58 @@
 class Sphere 
 {
 public:
-    Sphere();
+    Sphere()
+    {
+        x = 0;
+        y = 0;
+        z = 0;
+    };
 
     Sphere(float xCoords, float yCoords, float zCoords) 
     {
         vec = Vector3
         {
+            //is this supposed to be xCoords, yCoord, zCoords instead of storing them?
             x = xCoords,
             y = yCoords,
             z = zCoords,
         };
     };
 
+    float getX()
+    {
+        return vec.x;
+    };
+
+    void setX(float coord)
+    {
+        vec.x = coord;
+    };
+
+    float getY()
+    {
+        return vec.y;
+    };
+
+    void setY(float coord)
+    {
+        vec.y = coord;
+    };
+
+    float getZ()
+    {
+        return vec.z;
+    };
+
+    void setZ(float coord)
+    {
+        vec.z = coord;
+    };
+
 private:
-float x, y, z = 0;
+    float x, y, z;
     Vector3 vec;
+    std::vector<float> coords;
 };
 
 #endif
