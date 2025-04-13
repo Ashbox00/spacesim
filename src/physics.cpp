3716b7ea43dcaad862a76b96804a2b01b73088c8
rebaltes <rebaltes@gmail.com>
2025-04-13 01:16:57 -0700

    physics 2: satetellite boogaloo

--- src/physics.cpp
+++ src/physics.cpp
@@ -3,6 +3,8 @@
 #include <raymath.h>
 #include <cmath>
 
+const float G = 6.67e-11f;
+
 class Planet{
     public:
     float mass;
@@ -12,25 +14,43 @@ class Planet{
     
 };
 
-Planet* updatePlanets(Planet planets[])
+
+Planet* updatePlanets(Planet planets[], float timesec)
 {
     Planet sun = planets[0];
-    float G = 6.67e-11f; //placeholder gravatational constant(the real one is too small)
+    //placeholder gravatational constant(the real one is too small)
 
     for (int i = 1; i < sizeof(planets); i++)
     {
         //Getting the x, y, and z components of the force
         
         //float Fx = G*sun.mass*planets[i].mass / planets[i].pos.x; for single components
-        Vector3 force = Vector3Scale(planets[i].pos, G*sun.mass*planets[i].mass);
-
+        Vector3 force = Vector3Scale(Vector3Invert(planets[i].pos), (G*sun.mass)*planets[i].mass);
+        
         //Updating position
-        int time = 100000; //how many "seconds" between each "update"
-        planets[i].pos += planets[i].vel*time + force/planets[i].mass*pow(time, 2)/2;
+        planets[i].pos += planets[i].vel*timesec + force/planets[i].mass*pow(timesec, 2)/2;
 
         //updating velocity
-        planets[i].vel += force/planets[i].mass * time;
+        planets[i].vel += force/planets[i].mass * timesec;
     }
 
     return planets;
+}
+
+Planet satelliteStep(Planet planets[], Planet &satellite, float timesec)
+{
+    Vector3 force = {0,0,0};
+
+    for(int i = 0; i < sizeof(planets); i++)
+    {
+        force += Vector3Scale(Vector3Invert(satellite.pos - planets[i].pos), (G*planets[i].mass)*satellite.mass);
+    }
+    
+    //Updating position
+    satellite.pos += satellite.vel*timesec + force/satellite.mass*pow(timesec, 2)/2;
+
+    //updating velocity
+    satellite.vel += force/satellite.mass * timesec;
+
+    return satellite;
 }
