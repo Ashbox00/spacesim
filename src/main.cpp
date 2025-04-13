71dcbfc3af433f42389a7b6d1e1301b3945a64d7
Cameron <camonater62@gmail.com>
2025-04-12 20:51:39 -0700

    get curl building

--- src/main.cpp
+++ src/main.cpp
@@ -9,38 +9,38 @@ using namespace std;
 
 int main()
 {
-    InitWindow(640, 480, "Space Sim");
-    SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+    // InitWindow(640, 480, "Space Sim");
+    // SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
-    Camera3D camera = {
-        .position = Vector3{0, 0, -5},
-        .target = Vector3{0, 0, 0},
-        .up = Vector3{0, 1, 0},
-        .fovy = 60.0f,
-        .projection = CAMERA_PERSPECTIVE,
-    };
+    // Camera3D camera = {
+    //     .position = Vector3{0, 0, -5},
+    //     .target = Vector3{0, 0, 0},
+    //     .up = Vector3{0, 1, 0},
+    //     .fovy = 60.0f,
+    //     .projection = CAMERA_PERSPECTIVE,
+    // };
 
     cout << download("https://cgwe.st/") << endl;
 
-    while (!WindowShouldClose())
-    {
-        UpdateCamera(&camera, CAMERA_THIRD_PERSON);
+    // while (!WindowShouldClose())
+    // {
+    //     UpdateCamera(&camera, CAMERA_THIRD_PERSON);
 
-        BeginDrawing();
-        {
-            ClearBackground(BLACK);
+    //     BeginDrawing();
+    //     {
+    //         ClearBackground(BLACK);
 
-            BeginMode3D(camera);
-            {
-                // DrawSphere(Vector3Zero(), 3, GREEN);
-                DrawSphereEx(Vector3Zero(), 3, 100, 100, YELLOW);
-            }
-            EndMode3D();
+    //         BeginMode3D(camera);
+    //         {
+    //             // DrawSphere(Vector3Zero(), 3, GREEN);
+    //             DrawSphereEx(Vector3Zero(), 3, 100, 100, YELLOW);
+    //         }
+    //         EndMode3D();
 
-            DrawFPS(10, 10);
-        }
-        EndDrawing();
-    }
+    //         DrawFPS(10, 10);
+    //     }
+    //     EndDrawing();
+    // }
 
-    CloseWindow();
+    // CloseWindow();
 }
