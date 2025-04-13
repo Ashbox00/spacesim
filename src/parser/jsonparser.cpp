ad0211485b542b9036c5fa7509622fe27da9bb34
Gage Ashbaugh <119578367+Ashbox00@users.noreply.github.com>
2025-04-12 19:27:03 -0700

    jsonParser

--- src/parser/jsonparser.cpp
+++ src/parser/jsonparser.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
 #include <string>
 #include <json.hpp>
+#include <fstream>
+#include <sstream>
 
 
 using json = nlohmann::json;
+using namespace std;
+
+string readFileAsString(const string& filename) {
+    ifstream inFile(filename);
+    ostringstream ss;
+    ss << inFile.rdbuf();
+    return ss.str();
+}
+
+void Jparser(string readBuffer){
+    string readBuffer;
+    try {
+        auto parsed = json::parse(readBuffer);
+        auto vectors = parsed["result"]["vector"];
+        if (!vectors.empty()){
+            auto coords = vectors[0];
+            cout << "Coordinates" << endl;
+            cout << "X: " << coords["X"] << endl;
+            cout << "Y: " << coords["Y"] << endl;
+            cout << "Z: " << coords["Z"] << endl;
+        }
+        else {
+            cout << "No data" << endl;
+        }
+    }
+    catch (const exception& e){
+        cout << "Failed to parse JSON" << endl;
+    }
+}
+
+int main(){
+    Jparser(readFileAsString("test.json"));
+}
